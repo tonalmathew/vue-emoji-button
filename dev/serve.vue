b0b92718f1af4bdb495228ada908b6c1e2c8a3ab
tonalmathew <tonalmathew1999@gmail.com>
2021-08-19 12:00:08 +0530

    chor: initial setup :rocket:

---
+++ dev/serve.vue
@@ -0,0 +1,18 @@
+<script>
+import { defineComponent } from 'vue';
+// Uncomment import and local "components" registration if library is not registered globally.
+import { EmojiButton } from '@/entry.esm';
+
+export default defineComponent({
+  name: 'ServeDev',
+  components: {
+   EmojiButton,
+  }
+});
+</script>
+
+<template>
+  <div id="app">
+    <emoji-button />
+  </div>
+</template>
