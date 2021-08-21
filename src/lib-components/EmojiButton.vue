1469be41cfb0ddf8ca86c9fee196d8197e3d9503
tonalmathew <tonalmathew1999@gmail.com>
2021-08-21 13:00:59 +0530

    chore: add emoji api

--- src/lib-components/EmojiButton.vue
+++ src/lib-components/EmojiButton.vue
@@ -1,48 +1,34 @@
 <template>
-  <button class="btn-cta">{{ text }}</button>
+  <button ><img :src="emojiIcon" :height="height" /></button>
 </template>
 
 <script>
 export default {
-  name: "FccButton", // vue component name
+  name: "EmojiButton", // vue component name
   props: {
-    text: {
+    height: {
       type: String,
-      default: "Enter Button Text Here",
+      default: "20",
+    },
+    emoji: {
+      type: String,
+      default: "face_with_tears_of_joy",
     },
   },
-  data() {},
+  data() {
+    return{
+      emojiIcon: null,
+    };
+  },
+  mounted(){
+    const emojiName = this.$props.emoji
+     this.emojiIcon = `https://emojiapi.dev/api/v1/${emojiName}.svg`
+    console.log(this.$props.emoji);
+  },
+  methods:{
+    
+  },
 };
 </script>
 
-<style>
-.btn-cta {
-  background-color: #d0d0d5;
-  border-width: 3px;
-  border-color: #1b1b32;
-  border-radius: 0;
-  border-style: solid;
-  color: #1b1b32;
-  display: block;
-  margin-bottom: 0;
-  font-weight: normal;
-  text-align: center;
-  -ms-touch-action: manipulation;
-  touch-action: manipulation;
-  cursor: pointer;
-  white-space: nowrap;
-  padding: 6px 12px;
-  font-size: 18px;
-  line-height: 1.42857143;
-}
-
-.btn-cta:active:hover,
-.btn-cta:focus,
-.btn-cta:hover {
-  background-color: #1b1b32;
-  border-width: 3px;
-  border-color: #000;
-  background-image: none;
-  color: #f5f6f7;
-}
-</style>
+<style></style>
